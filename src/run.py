e630e53b0be3829e9154a85e8fa3233da0a5be51
Jacob Lee <jacoblee628@gmail.com>
2020-11-26 01:15:13 -0500

    Created file structure, uploaded data

---
+++ src/run.py
@@ -0,0 +1,7 @@
+# TODO: When necessary args are decided, maybe write an argparser?
+
+if __name__ == "__main__":
+    """Code that's run when run.sh or python src/run.py is called"""
+    print("Running script")
+    
+    print("Script finished")
