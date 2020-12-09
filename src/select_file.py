c3c4a6ab12b50a82847fa68b9e1045e8131160e6
liuyuhan1997 <15022573990@163.com>
2020-12-09 14:58:50 -0500

    added folder selecting

--- src/select_file.py
+++ src/select_file.py
@@ -45,8 +45,35 @@ class Application(Frame):
         self.ManualselectBtn=Button(self, text="select",command=self.selectManualPath)
         self.ManualselectBtn.grid(row=3,column=2)
 
+        #TraceMatrix file select
+        self.TraceMatrixpath=StringVar()
+        self.TraceMatrixnameLabel=Label(self, text="Trace Matrix file path:")
+        self.TraceMatrixnameLabel.grid(row=4,column=0)
+        self.TraceMatrixfileName=Entry(self, textvariable=self.TraceMatrixpath, width='60')
+        self.TraceMatrixfileName.grid(row=4,column=1)
+        self.TraceMatrixselectBtn=Button(self, text="select",command=self.selectTraceMatrixPath)
+        self.TraceMatrixselectBtn.grid(row=4,column=2)
+
+        #Rest api folder select
+        self.RestAPIpath=StringVar()
+        self.RestAPInameLabel=Label(self, text="RestAPI folder path:")
+        self.RestAPInameLabel.grid(row=5,column=0)
+        self.RestAPIfileName=Entry(self, textvariable=self.RestAPIpath, width='60')
+        self.RestAPIfileName.grid(row=5,column=1)
+        self.RestAPIselectBtn=Button(self, text="select",command=self.selectRestAPIPath)
+        self.RestAPIselectBtn.grid(row=5,column=2)
+
+        #Performance test results file select
+        self.performancepath=StringVar()
+        self.performancenameLabel=Label(self, text="Performance Test file path:")
+        self.performancenameLabel.grid(row=6,column=0)
+        self.performancefileName=Entry(self, textvariable=self.performancepath, width='60')
+        self.performancefileName.grid(row=6,column=1)
+        self.performanceselectBtn=Button(self, text="select",command=self.selectperformancePath)
+        self.performanceselectBtn.grid(row=6,column=2)
+
         self.startBtn=Button(self, text="Start Process", command=self.start)
-        self.startBtn.grid(row=5,column=1)
+        self.startBtn.grid(row=7,column=1)
 
     def selectSRSPath(self):
         SRSfile_path = fd.askopenfilename()
@@ -72,7 +99,7 @@ class Application(Frame):
         ObsoleteSRSfile_path = fd.askopenfilename()
         self.ObsoleteSRSpath.set(ObsoleteSRSfile_path)
         ObsoleteSRSname=self.ObsoleteSRSfileName.get()
-        if 'Obsolete' in ObsoleteSRSname:
+        if 'obsolete' in ObsoleteSRSname.lower():
             messagebox.showinfo("Message",ObsoleteSRSname)
         else:
             messagebox.showwarning("Message",'Wrong name!')
@@ -82,11 +109,44 @@ class Application(Frame):
         Manualfile_path = fd.askopenfilename()
         self.Manualpath.set(Manualfile_path)
         Manualname=self.ManualfileName.get()
-        if 'Manual' in Manualname:
+        if 'manual' in Manualname.lower():
             messagebox.showinfo("Message",Manualname)
         else:
             messagebox.showwarning("Message",'Wrong name!')
             self.Manualpath.set('')
+
+    def selectTraceMatrixPath(self):
+        TraceMatrixfile_path = fd.askopenfilename()
+        self.TraceMatrixpath.set(TraceMatrixfile_path)
+        TraceMatrixname=self.TraceMatrixfileName.get()
+        if 'trace matrix' in TraceMatrixname.lower():
+            messagebox.showinfo("Message",TraceMatrixname)
+        else:
+            messagebox.showwarning("Message",'Wrong name!')
+            self.TraceMatrixpath.set('')
+    
+    def selectRestAPIPath(self):
+        RestAPIfile_path = fd.askdirectory()
+        self.RestAPIpath.set(RestAPIfile_path)
+        RestAPIname=self.RestAPIfileName.get()
+        if 'rest' in RestAPIname.lower():
+            messagebox.showinfo("Message",RestAPIname)
+        else:
+            messagebox.showwarning("Message",'Wrong name!')
+            self.RestAPIpath.set('')
+
+    def selectperformancePath(self):
+        performancefile_path = fd.askopenfilename()
+        self.performancepath.set(performancefile_path)
+        performancename=self.performancefileName.get()
+        if 'performance' in performancename.lower():
+            messagebox.showinfo("Message",performancename)
+        else:
+            messagebox.showwarning("Message",'Wrong name!')
+            self.performancepath.set('')
+ 
+
+
     
     def start(self):
         messagebox.showinfo('Message','waiting for further integration')
