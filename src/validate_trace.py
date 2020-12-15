b7d27a752f6084ba048404946615a4a4cabbfc02
ppuvvadi <70478780+ppuvvadi@users.noreply.github.com>
2020-12-14 16:01:06 -0800

    Update validate_trace.py

--- src/validate_trace.py
+++ src/validate_trace.py
@@ -75,7 +75,18 @@ def check_srs_has_test(trace, prd_prefix, srs_prefix):
     
     # Get df of rows where SRS exists but test does not
     invalid = num_unique.loc[num_unique["Test Name"] == 0]
+    invalid = pd.DataFrame(invalid, columns=['SRS ID', 'Test Name'])
+    invalid_df = pd.DataFrame()
+    for val in invalid['SRS ID']:
+        invalid_df = invalid_df.append(trace[trace['SRS ID'] == val])
+    invalid_df = invalid_df.sort_index()
+        
     valid = num_unique.loc[num_unique["Test Name"] != 0]
+    valid = pd.DataFrame(valid, columns=['SRS ID', 'Test Name'])
+    valid_df = pd.DataFrame()
+    for val in valid['SRS ID']:
+        valid_df = valid_df.append(trace[trace['SRS ID'] == val])
+    valid_df = valid_df.sort_index()
 
     return valid, invalid
     
